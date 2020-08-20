eddd1730e820765220d2a8b1b9988c3d14339609
shreytheshreyas <shreyaskumar52@gmail.com>
2020-08-20 19:05:02 +0800

    Level-0 Greet

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,9 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(logo);
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?\n");
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
